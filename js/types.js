fa1e1ee2031f6a7969b0f7ef5af504706b7118e8
fedeparrilla10 <federicoivanpd@gmail.com>
2023-05-10 20:09:32 +0300

    Updating Pokédex!

---
+++ js/types.js
@@ -0,0 +1,17 @@
+export const pokeTypes = {
+  grass: "#2DFF66",
+  fire: "#FF1B1B",
+  water: "#00DCFF",
+  bug: "#428530",
+  normal: "#FFFEB0",
+  poison: "#6F0078",
+  electric: "#F6DC00",
+  ground: "#845600",
+  fairy: "#F000FF",
+  fighting: "#810000",
+  psychic: "#CA41BF",
+  ghost: "#784A74",
+  rock: "#888888",
+  ice: "#BAFCFF",
+  dragon: "#6C00FF",
+};
